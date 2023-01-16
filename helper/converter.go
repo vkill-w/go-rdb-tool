451d1cce928a11c9879c7bacb07aa3f0ed8e9965
vkill-w <68580340+vkill-w@users.noreply.github.com>
2023-01-16 16:57:56 +0800

    Update converter.go
    add rdb file to es

--- helper/converter.go
+++ helper/converter.go
@@ -7,7 +7,81 @@ import (
 	"github.com/hdt3213/rdb/core"
 	"github.com/hdt3213/rdb/model"
 	"os"
+	jsonvalue "github.com/Andrew-M-C/go.jsonvalue"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esutil"
 )
+
+
+
+// ToES read rdb file and convert to elasticsearch
+func ToES(rdbFilename string, esUrl string, options ...interface{}) error {
+	if rdbFilename == "" {
+		return errors.New("src file path is required")
+	}
+	if esUrl == "" {
+		return errors.New("output file path is required")
+	}
+	// open file
+	rdbFile, err := os.Open(rdbFilename)
+	if err != nil {
+		return fmt.Errorf("open rdb %s failed, %v", rdbFilename, err)
+	}
+	// close file
+	defer func() {
+		_ = rdbFile.Close()
+	}()
+
+	//创建es client
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			esUrl,
+		},
+	}
+	es, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		return fmt.Errorf("create  es client  failed, %v", err)
+	}
+	// create decoder
+	var dec decoder = core.NewDecoder(rdbFile)
+	var regexOpt RegexOption
+	for _, opt := range options {
+		switch o := opt.(type) {
+		case RegexOption:
+			regexOpt = o
+		}
+	}
+	if regexOpt != nil {
+		dec, err = regexWrapper(dec, *regexOpt)
+		if err != nil {
+			return err
+		}
+	}
+	// parse rdb into es
+	index := "redis_rc_bop"
+	bi, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+		Index:         index,            // The default index name
+		Client:        es,               // The Elasticsearch client
+		NumWorkers:    4,                // The number of worker goroutines
+		FlushBytes:    500000,           // The flush threshold in bytes
+		FlushInterval: 30 * time.Second, // The periodic flush interval
+	})
+	err = dec.Parse(func(object model.RedisObject) bool {
+		//ES存储对象拼装
+		addDataToBulkIndexer(object, bi)
+		return true
+	})
+	if err := bi.Close(context.Background()); err != nil {
+		log.Fatalf("Unexpected error: %s", err)
+	}
+	biStats := bi.Stats()
+	biStatsJson, err := json.Marshal(biStats)
+	log.Fatalf("biStats NumAdded is %s", biStatsJson)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 // ToJsons read rdb file and convert to json file
 func ToJsons(rdbFilename string, jsonFilename string, options ...interface{}) error {
